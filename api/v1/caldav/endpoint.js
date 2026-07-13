ab3fec32d3ef2229e24083837655c35d41eaea1d
St4lV <152074070+St4lV@users.noreply.github.com>
2026-07-13 18:26:17 +0200

    Revert "0.1.30 - bugfixs"
    This reverts commit c648ce5fc69d162937bfdb0dc9bb962a3d8948d1.

--- api/v1/caldav/endpoint.js
+++ api/v1/caldav/endpoint.js
@@ -19,7 +19,7 @@ router.post('/', async(req, res)=>{
 router.put('/', async(req, res)=>{
 	const { event, new_event } = req.body
 	const calendar = new Calendar(event);
-	const result = await calendar.modify(new_event.start, new_event.end, new_event.summary, new_event.description, new_event.location, new_event.allDay);
+	const result = await calendar.modify(new_event);
 	return res.status(result.code).json(result);
 });
 
